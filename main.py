e448ac96d3553132e9df47a95d18fbb9c81ff307
Aditya Datta <ADITYADATTAOFFICIAL@GMAIL.COM>
2024-03-28 09:06:43 +0530

    'last-commit'

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Variable to store the last answer
+last_answer = None
+
 # Allow CORS for all origins
 app.add_middleware(
     CORSMiddleware,
@@ -12,10 +15,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def get_answer():
-    return {"hi": "pi"}
-@app.get("/api/answer")
-async def get_answer():
-    return {"answer": "yes"}
+@app.get("/api/answer/{user}")
+async def get_answer(user: str):
+    global last_answer
+    if user == "yes":
+        last_answer = {"answer": "yes"}
+        return last_answer
+    if user == "no":
+        last_answer = {"answer": "no"}
+        return last_answer
 
+@app.get("/api/answer/")
+async def get_last_answer():
+    global last_answer
+    if last_answer:
+        return last_answer
+    else:
+        return {"error": "No answer has been set yet"}
